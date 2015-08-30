f11476bc7eda4db6a2a736ebf4d0030b701b156e
ricardoteles <ricardo.oliveira.teles@gmail.com>
2015-08-30 20:24:58 -0300

    Começamos a implementar o fisrtCome-firstServed

--- ep1.c
+++ ep1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+// #include <semaphore.h>
+
 #define NUM_PROCESSOS 8
 
 typedef struct{
@@ -8,29 +12,41 @@ typedef struct{
 	int p;
 } PROC;
 
-
-/************* ASSINATURA DAS FUNCOES ******************/
-void imprime(PROC entradaProcessos[]);
-void parserEntrada(int argc, char* argv[]);
-void inicializaThreads(PROC entradaProcessos[]);
-void *processo(void *a);
+struct timeval inicio, fim;
 
 /************* VARIAVEIS GLOBAIS ***********************/
 int numEscalonamento = 0;
 FILE* arqEntrada = NULL;  
 FILE* arqSaida = NULL;
 char d = '0';
+
+/************* ASSINATURA DAS FUNCOES ******************/
+void parserEntrada(int argc, char* argv[]);
+void inicializaProcessos(PROC entradaProcessos[]);
+void *processo(void *a);
+int compare_arrive(const void *a,const void *b);
+float tempoDesdeInicio();
+void imprime(PROC entradaProcessos[]);
+
 /*******************************************************/
 
 int main(int argc, char* argv[]) {
+	gettimeofday(&inicio, NULL);
+	
 	PROC entradaProcessos[NUM_PROCESSOS];				// TODO: criar um vetor com tamanho variavel
 	pthread_t procs[NUM_PROCESSOS];
 	long i;
 
 	parserEntrada(argc, argv);
-	inicializaThreads(entradaProcessos);
+	inicializaProcessos(entradaProcessos);
 	
+	if(numEscalonamento == 1){
+		qsort(entradaProcessos,NUM_PROCESSOS,sizeof(PROC),compare_arrive);
+	}
+
 	for(i = 0; i < NUM_PROCESSOS; i++){
+		while(tempoDesdeInicio() < entradaProcessos[i].t0);
+		
 		if(pthread_create(&procs[i], NULL, processo, (void *) &entradaProcessos[i])){
             printf("\n ERROR creating thread %ld\n", i);
             exit(1);
@@ -44,27 +60,25 @@ int main(int argc, char* argv[]) {
         }
     }
 
-	imprime(entradaProcessos);
 	fclose(arqEntrada);
 
+	// printf("%f seg\n\n", tempoDesdeInicio());
 	return 0;
 }
 
 void *processo(void *a){
 	PROC *val = (PROC *) a;
 
-	printf("%s\n", val->nome);
-	printf("%f\n", val->t0);
+	printf("Ola eu sou o processo:  %s\n", val->nome);
 	
 	return NULL;
 }
 
-void inicializaThreads(PROC entradaProcessos[]){
+void inicializaProcessos(PROC entradaProcessos[]){
 	int i = 0;
 
 	while (fscanf(arqEntrada,"%f %s %f %f %d", &entradaProcessos[i].t0, entradaProcessos[i].nome, 
-		&entradaProcessos[i].dt, &entradaProcessos[i].deadline, &entradaProcessos[i].p) != EOF) {
-		
+		&entradaProcessos[i].dt, &entradaProcessos[i].deadline, &entradaProcessos[i].p) != EOF) {		
 		i++;
 	}
 }
@@ -86,16 +100,36 @@ void parserEntrada(int argc, char* argv[]){
 	}
 }
 
+int compare_arrive(const void *a,const void *b) {
+	PROC *x = (PROC *) a;
+	PROC *y = (PROC *) b;
+
+	if (x->t0 < y->t0) return -1;
+	else if (x->t0 > y->t0) return 1; 
+
+	return 0;
+}
+
 /************* FUNCOES APENAS PARA TESTE *************/
+
+float tempoDesdeInicio(){
+	float timedif;
+
+	gettimeofday(&fim, NULL);
+	timedif = (float)(fim.tv_sec - inicio.tv_sec);
+	timedif += (float)(fim.tv_usec - inicio.tv_usec)/1000000;
+
+	return timedif;
+}
 
 void imprime(PROC entradaProcessos[]){
 	int i;
 
 	for (i = 0; i < 8; i++) {
 		fprintf(arqSaida ,"Nome: %s\n", entradaProcessos[i].nome);
-		fprintf(arqSaida ,"t0: %f\n", entradaProcessos[i].t0);
-		fprintf(arqSaida ,"dt: %f\n", entradaProcessos[i].dt);
-		fprintf(arqSaida ,"deadline: %f\n", entradaProcessos[i].deadline);
-		fprintf(arqSaida ,"p: %d\n\n", entradaProcessos[i].p);
+		// fprintf(arqSaida ,"t0: %f\n", entradaProcessos[i].t0);
+		// fprintf(arqSaida ,"dt: %f\n", entradaProcessos[i].dt);
+		// fprintf(arqSaida ,"deadline: %f\n", entradaProcessos[i].deadline);
+		// fprintf(arqSaida ,"p: %d\n\n", entradaProcessos[i].p);
 	}
 }
